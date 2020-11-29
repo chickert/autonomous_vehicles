16844d79237a47ff530465539eefa5303cb79ed0
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2020-11-29 10:54:38 -0500

    implemented third extension

--- code/extensions.py
+++ code/extensions.py
@@ -7,7 +7,7 @@ from controllers import *
 
 
 def extension_one(num_vehicles):
-    means_after = []
+    mean_system_vels = []               # mean velocity across all vehicles and all timesteps after AV activation
     variance_across_vehicles = []       # (mean is taken across 22 std devs -- one std dev from each vehicle)
     variance_across_timesteps = []      # (mean is taken across all timesteps -- one std dev from each timestep)
 
@@ -39,14 +39,14 @@ def extension_one(num_vehicles):
         steps_after = range(env.av_activate, env.step)
         speeds_after = env.get_vehicle_vel_table(steps_after)
 
-        # Store std dev and mean of each vehicle's velocity averaged across all vehicles BEFORE controller was activated
-        means_after.append(speeds_after.mean(axis=0).mean())
+        # Store std dev and mean of each vehicle's velocity averaged across all vehicles after controller was activated
+        mean_system_vels.append(speeds_after.mean(axis=0).mean())           # mean velocity across all vehicles and all timesteps after AV activation
         variance_across_vehicles.append(speeds_after.std(axis=0).mean())    # (mean is taken across 22 std devs -- one std dev from each vehicle)
         variance_across_timesteps.append(speeds_after.std(axis=1).mean())   # (mean is taken across all timesteps -- one std dev from each timestep)
         pct_avs.append((num_avs / num_vehicles)*100)
 
     # Plot results
-    plt.plot(pct_avs, means_after)
+    plt.plot(pct_avs, mean_system_vels)
     plt.xlabel("% of Vehicles that are AVs")
     plt.ylabel("Mean Velocity (after AV activation)\n across All Vehicles")
     plt.title("System's Mean Velocity\n as Proportion of AVs Increases")
@@ -67,9 +67,9 @@ def extension_one(num_vehicles):
 
 def extension_two(a_sigma_start, a_sigma_stop, a_sigma_granularity, b_sigma_start, b_sigma_stop, b_sigma_granularity):
     # Initialize lists to store results
-    mean_system_vels = []
-    variance_across_vehicles = []
-    variance_across_timesteps = []
+    mean_system_vels = []           # mean velocity across all vehicles and all timesteps after AV activation
+    variance_across_vehicles = []   # (mean is taken across 22 std devs -- one std dev from each vehicle)
+    variance_across_timesteps = []  # (mean is taken across all timesteps -- one std dev from each timestep)
     # Define grid of values to scan over
     a_sigma_grid = np.arange(a_sigma_start, a_sigma_stop, a_sigma_granularity)
     b_sigma_grid = np.arange(b_sigma_start, b_sigma_stop, b_sigma_granularity)
@@ -102,8 +102,8 @@ def extension_two(a_sigma_start, a_sigma_stop, a_sigma_granularity, b_sigma_star
             speeds_after = env.get_vehicle_vel_table(steps_after)
 
             # Store std dev and mean of each vehicle's velocity averaged
-            # across all vehicles BEFORE controller was activated
-            mean_system_vels.append(speeds_after.mean(axis=0).mean())
+            # across all vehicles after controller was activated
+            mean_system_vels.append(speeds_after.mean(axis=0).mean())   # mean velocity across all vehicles and all timesteps after AV activation
             variance_across_vehicles.append(
                 speeds_after.std(axis=0).mean())  # (mean is taken across 22 std devs -- one std dev from each vehicle)
             variance_across_timesteps.append(speeds_after.std(
@@ -132,14 +132,71 @@ def plot_3d(a_sigma_grid, b_sigma_grid, z_list, title, x_axis_label=r'$a_{\sigma
     plt.show()
 
 
-if __name__ == '__main__':
-    import warnings
-    warnings.filterwarnings("ignore", category=UserWarning)
+def extension_three(max_sigma_pct, granularity):
+    max_sigma_pct = max_sigma_pct / 100
+    granularity = granularity / 100
+    sigma_pcts = np.arange(0., max_sigma_pct, granularity)
+    mean_system_vels = []               # mean velocity across all vehicles and all timesteps after AV activation
+    variance_across_vehicles = []       # (mean is taken across 22 std devs -- one std dev from each vehicle)
+    variance_across_timesteps = []      # (mean is taken across all timesteps -- one std dev from each timestep)
 
-    extension_two(a_sigma_start=0.,
-                  a_sigma_stop=0.05,
-                  a_sigma_granularity=0.005,
-                  b_sigma_start=0.,
-                  b_sigma_stop=1.0,
-                  b_sigma_granularity=0.1,
-                  )
+    print(f"Running {len(sigma_pcts)} simulations")
+    counter = 0
+    for sigma_pct in sigma_pcts:
+
+        # Increment counter
+        counter += 1
+        if counter % 20 == 0:
+            print(f'On simulation #{counter}')
+
+        # Build env
+        env = RingRoad(
+            num_vehicles=22,  # The vechicles at index 0 is an A.V.
+            ring_length=230.0,  # The road is a cicle.
+            starting_noise=4.0,  # Uniformly add noise to starting positions.
+            temporal_res=0.3,  # Set the size of simulation steps (seconds).
+            av_activate=30,  # Set when the PID controller is activated.
+            seed=286,  # Set a random seed.
+            uncertain_avs=True,
+            sigma_pct=sigma_pct
+        )
+
+        # Run each simulation for set number of time steps:
+        total_time = 50  # In seconds.
+        total_steps = int(np.ceil(total_time / env.dt))
+        env.run(steps=total_steps)
+
+        # Collect and system metrics for plotting:
+        steps_after = range(env.av_activate, env.step)
+        speeds_after = env.get_vehicle_vel_table(steps_after)
+
+        # Store std dev and mean of each vehicle's velocity averaged across all vehicles after controller was activated
+        mean_system_vels.append(speeds_after.mean(axis=0).mean())           # mean velocity across all vehicles and all timesteps after AV activation
+        variance_across_vehicles.append(speeds_after.std(axis=0).mean())    # (mean is taken across 22 std devs -- one std dev from each vehicle)
+        variance_across_timesteps.append(speeds_after.std(axis=1).mean())   # (mean is taken across all timesteps -- one std dev from each timestep)
+
+    # Plot results
+    sigma_pcts = sigma_pcts * 100
+    plt.plot(sigma_pcts, mean_system_vels)
+    plt.xlabel("Sensor Noise\n(% of $\Delta_x$ and lead vehicle velocity measurements)")
+    plt.ylabel("Mean Velocity (after AV activation)\n across All Vehicles")
+    plt.title("System's Mean Velocity\n as Uncertainty Increases")
+    plt.show()
+
+    plt.plot(sigma_pcts, variance_across_vehicles)
+    plt.xlabel("Sensor Noise\n(% of $\Delta_x$ and lead vehicle velocity measurements)")
+    plt.ylabel("Velocity Std. Dev. (after AV activation)\n across All Vehicles")
+    plt.title("Mean across Individual Vehicle Velocity Std Devs\n as Uncertainty Increases")
+    plt.show()
+
+    plt.plot(sigma_pcts, variance_across_timesteps)
+    plt.xlabel("Sensor Noise\n(% of $\Delta_x$ and lead vehicle velocity measurements)")
+    plt.ylabel("Velocity Std. Dev. (after AV activation)\n across All Timesteps")
+    plt.title("Mean across Individual Timestep System Velocity Std Devs\n as Uncertainty Increases")
+    plt.show()
+
+
+# if __name__ == '__main__':
+#     import warnings
+#     warnings.filterwarnings("ignore", category=UserWarning)
+#     extension_three(max_sigma_pct=40, granularity=10)
