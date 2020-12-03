1baf04b15c4e8d344ee5b693c383f8ecc1889bb6
Gabriel <gpestre@users.noreply.github.com>
2020-12-02 19:46:06 -0500

    New tuning.

--- code/dqn/train.py
+++ code/dqn/train.py
@@ -25,26 +25,29 @@ LAYER_2_NODES = 256
 GAMMA = 0.999
 # EPS_DECAY_RATE = 0.999985 # (orange, lime green)
 # EPS_DECAY_RATE = 0.999977 # (teal)
-EPS_DECAY_RATE = 0.999977 # (magenta, green, light blue, pink, gold, grey, peach, dark blue)
+# EPS_DECAY_RATE = 0.999977 # (magenta, green, light blue, pink, gold, grey, peach, dark blue, black)
 # EPS_DECAY_RATE = 0.999700 # (brown)
+EPS_DECAY_RATE = 0.999980
 # LR = 1e-4 # (green + priors)
-LR = 5e-4 # (light blue, lime green, brown, grey)
+# LR = 5e-4 # (light blue, lime green, brown, grey, black)
 # LR = 3e-4 # (peach, dark blue)
 # LR = 1e-3 # (pink)
 # LR = 6e-5 # (gold)
+LR = 3e-4
 BATCH_SIZE = 256
-NUM_EPISODES = 6_000
+NUM_EPISODES = 200_000
 MAX_TIMESTEPS = 200
 # REPLAY_MEMORY_SIZE = 20_000 # (orange)
 # REPLAY_MEMORY_SIZE = 50_000 # (teal)
-REPLAY_MEMORY_SIZE = 50_000 # (magenta, green, light blue, pink, gold, lime green, brown, grey, peach)
+REPLAY_MEMORY_SIZE = 250_000 # (magenta, green, light blue, pink, gold, lime green, brown, grey, peach, black)
 # REPLAY_MEMORY_SIZE = 80_000 # (dark blue)
 # TIMESTEPS_BEFORE_TARGET_NETWORK_UPDATE = 2_000 # (orange)
 # TIMESTEPS_BEFORE_TARGET_NETWORK_UPDATE = 5_000 # (teal)
 # TIMESTEPS_BEFORE_TARGET_NETWORK_UPDATE = 2_000 # (magenta, light blue)
 # TIMESTEPS_BEFORE_TARGET_NETWORK_UPDATE = 1_000 # (green)
-TIMESTEPS_BEFORE_TARGET_NETWORK_UPDATE = 3_000 # (light blue, pink, gold, lime green, brown, grey, dark blue)
-WANDB_TSTEP = 50
+# TIMESTEPS_BEFORE_TARGET_NETWORK_UPDATE = 3_000 # (light blue, pink, gold, lime green, brown, grey, dark blue, black)
+TIMESTEPS_BEFORE_TARGET_NETWORK_UPDATE = 6_000 # (light blue, pink, gold, lime green, brown, grey, dark blue, black)
+WANDB_TSTEP = TIMESTEPS_BEFORE_TARGET_NETWORK_UPDATE
 REWARD_SCALING = 1./100.
 SEED = 1
 SAVE_PATH = './saved-models/trained_model_'
@@ -52,11 +55,16 @@ SAVE_PATH = './saved-models/trained_model_'
 #####################
 
 
-def train(agent, gamma, list_of_rewards_for_all_episodes, env, tstep, wandb_tstep):
-    if len(agent.replay_memory.memory) <= agent.batch_size:
+def train(agent, gamma, list_of_rewards_for_all_episodes, env, wandb_tstep):
+    # if len(agent.replay_memory.memory) <= agent.batch_size:
+    #     return
+    if len(agent.replay_memory.memory) < REPLAY_MEMORY_SIZE:
+        if len(agent.replay_memory.memory) % 5_000 == 0:
+            print(f'Replay Memory now has {len(agent.replay_memory.memory)} transitions')
         return
 
-    if len(agent.replay_memory.memory) == agent.batch_size + 1:
+    # if len(agent.replay_memory.memory) == agent.batch_size + 1:
+    if len(agent.replay_memory.memory) == REPLAY_MEMORY_SIZE:
         print(f"""Replay memory now has {len(agent.replay_memory.memory)} transitions,
             which is sufficient to begin training.
             """)
@@ -82,7 +90,7 @@ def train(agent, gamma, list_of_rewards_for_all_episodes, env, tstep, wandb_tste
     # The torch.sum() component is to select the q_vals ONLY for the action taken
     # without having to use a loop
     loss = ((rewards + gamma * masks[:, 0] * next_state_q_vals - torch.sum(q_vals * actions_one_hot, -1))**2).mean()
-    if tstep % wandb_tstep == 0:
+    if agent.current_timestep_number % wandb_tstep == 0:
         wandb.log({'Loss': loss.detach().item(),
                    'Epsilon': agent.epsilon,
                    'Average reward over last 100 episodes': np.mean(list_of_rewards_for_all_episodes[-100:])},
@@ -105,7 +113,7 @@ def main():
                    'ring_length': 100.0,
                    'starting_noise': 1.0,
                    'temporal_res': 0.5,
-                   'av_activate': MAX_TIMESTEPS,
+                   'av_activate': 0,
                    'seed': 286,
                    'learning_mode': True}
     past_steps = 3
@@ -113,6 +121,7 @@ def main():
     # agent_commands=[-5.0, -1.0, 0.0, 1.0, 5.0]  # (grey)
     # agent_commands=[-4.0, -1.0, -0.1, 0.0, 0.1, 1.0, 4.0] # (peach)
     # agent_commands=[-2.0, -0.1, 0.0, 0.1, 2.0]  # (dark blue)
+    # agent_commands=[-0.5, -0.1, 0.0, 0.1, 0.5],  # (black)
     agent_commands_string = "|".join([str(comm) for comm in agent_commands])  # W&B charts can't group by lists.
     config = {
         **road_params,
@@ -134,7 +143,10 @@ def main():
     }
 
     # Initialize wandb
-    wandb.init(project="cs286", name="tuning_dqn-avs", config=config)
+    wandb.init(
+        project="cs286", name="tuning_dqn-avs", config=config,
+        note="Fill a large replay memory with random moves before beginning training and epsilon decay.",
+    )
 
     # Define a ring road environment:
     road = RingRoad(**road_params)
@@ -163,7 +175,8 @@ def main():
         target_network=target_network,
         replay_memory=replay_memory,
         batch_size=BATCH_SIZE,
-        decay_rate=EPS_DECAY_RATE
+        decay_rate=EPS_DECAY_RATE,
+        decay_starts_at=REPLAY_MEMORY_SIZE,
     )
 
     # full_path = f"{SAVE_PATH}{wandb.run.id}_q_network.pt"
@@ -199,7 +212,6 @@ def main():
                   gamma=GAMMA,
                   list_of_rewards_for_all_episodes=list_of_rewards_for_all_episodes,
                   env=env,
-                  tstep=agent.current_timestep_number,
                   wandb_tstep=WANDB_TSTEP)
 
             observation = next_observation
